dc81079fab17b9e091db4acfe8ad44d662fb3c11
KhanhTIMN <khanhlcbb@gmail.com>
2023-10-13 04:23:11 +0700

    day 1

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    do {
+        const mid = Math.floor(low + (high - low) / 2);
+        if (needle < haystack[mid]) {
+            high = mid;
+        } else if (needle > haystack[mid]) {
+            low = mid + 1;
+        } else if (needle === haystack[mid]) {
+            return true;
+            break;
+        }
+    } while (low < high);
+
+    return false;
 }
