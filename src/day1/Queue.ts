584371f2e787bccc966df09c2d8543eec721b01b
KhanhTIMN <khanhlcbb@gmail.com>
2023-10-17 01:20:36 +0700

    queue not finish

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,27 @@
+type QNode<T> = {
+    value: T;
+    next?: QNode<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: QNode<T>;
+    private tail?: QNode<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        this.tail?.next?.value = item: T;
+    }
 
-}
     deque(): T | undefined {
 
-}
-    peek(): T | undefined {
 
-}
+    }
+    peek(): T | undefined {
+        return this.head?.value;
+    }
 }
