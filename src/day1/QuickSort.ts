19128592924ce0becc85209778bb30da469dc1a9
phmtrang <trangphamlclc@gmail.com>
2023-11-06 18:03:30 +0700

    test quick sort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,27 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], low: number, high: number): void {
+    if (low >= high) {
+        return;
+    }
+    qs(arr, low, pt(arr, low, high) - 1);
+    qs(arr, pt(arr, low, high) + 1, high);
+}
 
+function pt(arr: number[], low: number, high: number): number {
+    const pivot = Math.floor((low + (high - low)) / 2);
+
+    let idx = low - 1;
+
+    for (let i = low; i < high; i++) {
+        idx++;
+        const temp = arr[i];
+        arr[i] = arr[idx];
+        arr[idx] = temp;
+    }
+
+    idx++;
+
+    return idx;
+}
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length);
 }
