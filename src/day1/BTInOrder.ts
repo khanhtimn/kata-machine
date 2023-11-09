635a584461069bd6aa26da113383935c9056ece8
phmtrang <trangphamlclc@gmail.com>
2023-11-09 14:32:25 +0700

    BTS

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,20 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function recurseTree(
+    node: BinaryNode<number> | null,
+    path: number[],
+): number[] {
+    if (!node) {
+        return path;
+    }
 
+    recurseTree(node.left, path);
+
+    path.push(node.value);
+
+    recurseTree(node.right, path);
+
+    return path;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return recurseTree(head, []);
 }
