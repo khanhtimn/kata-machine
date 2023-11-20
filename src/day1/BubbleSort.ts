8e6215254d2570a715d98a0b53605526fa0aa0e2
KhanhTIMN <khanhlcbb@gmail.com>
2023-11-20 16:31:21 +0700

    better bubble

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,11 +1,26 @@
+// export default function bubble_sort(arr: number[]): void {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr.length - i - 1; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 let temp = arr[j + 1];
+//                 arr[j + 1] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         }
+//     }
+// }
+
 export default function bubble_sort(arr: number[]): void {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                let temp = arr[j + 1];
-                arr[j + 1] = arr[j];
-                arr[j] = temp;
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+                swapped = true;
             }
         }
-    }
+    } while (swapped);
 }
