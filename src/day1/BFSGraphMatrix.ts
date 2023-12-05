aa0b436196d3feb88e3ec7b3dcf7f28306278f2d
KhanhTIMN <khanhlcbb@gmail.com>
2023-12-05 18:31:41 +0700

    changed files

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -2,25 +2,4 @@ export default function bfs(
     graph: WeightedAdjacencyMatrix,
     source: number,
     needle: number,
-): number[] | null {
-    const seen = new Array(graph.length).fill(false);
-    const prev = new Array(graph.length).fill(-1);
-    seen[source] = true;
-    const q: number[] = [source];
-    do {
-        const curr = q.shift() as number;
-        if (curr === needle) {
-            break;
-        }
-        for (let i = 0; i < graph.length; i++) {
-            if (seen[curr] === true) {
-                continue;
-            }
-            seen[curr] = true;
-            prev[curr] = curr;
-            q.push(curr);
-        }
-    } while (q.length);
-    return q;
-}
-
+): number[] | null {}
--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -2,4 +2,24 @@ export default function dfs(
     graph: WeightedAdjacencyList,
     source: number,
     needle: number,
-): number[] | null {}
+): number[] | null {
+    const seen = new Array(graph.length).fill(false);
+    const prev = new Array(graph.length).fill(-1);
+    seen[source] = true;
+    const q: number[] = [source];
+    do {
+        const curr = q.shift() as number;
+        if (curr === needle) {
+            break;
+        }
+        for (let i = 0; i < graph.length; i++) {
+            if (seen[curr] === true) {
+                continue;
+            }
+            seen[curr] = true;
+            prev[curr] = curr;
+            q.push(curr);
+        }
+    } while (q.length);
+    return q;
+}
